fbc4a69f35b8606f1520a82663d2ddc08f2354c1
J. Istiak <xabid.in09@gmail.com>
2023-03-06 17:03:44 +0100

    pdf generate function

--- main.py
+++ main.py
@@ -25,3 +25,21 @@ def create_github_issue(title, body):
 
 # Call the create_github_issue function with the user's input
 create_github_issue('New feature request', 'Please add a feature that allows me to do XYZ')
+
+
+
+# create a function to generate a PDF file
+def generate_pdf(topic, resources):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=16)
+    pdf.cell(200, 10, txt=f"Recommended Resources for {topic}", ln=1, align="C")
+    pdf.cell(200, 10, txt=" ", ln=1)
+    for i, resource in enumerate(resources):
+        pdf.cell(200, 10, txt=f"{i+1}. {resource}", ln=1, link=resource)
+        pdf.cell(200, 5, txt=" ", ln=1)
+    pdf.cell(200, 10, txt=" ", ln=1)
+    pdf.output("resources.pdf", "F")
+    with open("resources.pdf", "rb") as pdf_file:
+        b64 = base64.b64encode(pdf_file.read()).decode()
+    return b64
