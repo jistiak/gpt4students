c21d3c786bf911a0e087ffd11084ebf17702d374
J. Istiak <xabid.in09@gmail.com>
2023-03-06 16:59:30 +0100

    script for github issue for features

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+# Define the GitHub API endpoint for issues
+issues_url = 'https://api.github.com/repos/<your-username>/<your-repo>/issues'
+
+# Set the headers for the API request, with your personal access token
+headers = {
+    'Authorization': f'Token <your-access-token>',
+    'Accept': 'application/vnd.github.v3+json'
+}
+
+# Define a function to create a new issue on GitHub
+def create_github_issue(title, body):
+    # Define the JSON payload for the API request
+    payload = {
+        'title': title,
+        'body': body
+    }
+    # Send the API request to create the new issue
+    response = requests.post(issues_url, headers=headers, json=payload)
+    if response.status_code == 201:
+        print('Issue created successfully')
+    else:
+        print('Error creating issue:', response.text)
+
+# Call the create_github_issue function with the user's input
+create_github_issue('New feature request', 'Please add a feature that allows me to do XYZ')
